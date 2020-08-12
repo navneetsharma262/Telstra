3d7d61148ade6f5ebac7976aad03ebb1ea4842ed
navneetsharma <Navneetsharma262@gmail.com>
2020-08-12 23:09:00 +0530

    Api call made in viewModel to fetch the data using coroutine.

--- app/src/main/java/com/nav/telstra/features/feedlist/view/FeedListFragment.kt
+++ app/src/main/java/com/nav/telstra/features/feedlist/view/FeedListFragment.kt
@@ -5,9 +5,19 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.lifecycle.ViewModelProviders
 import com.nav.telstra.R
+import com.nav.telstra.features.feedlist.viewModel.FeedListViewModel
 
-class FeedListFragment : Fragment(){
+class FeedListFragment : Fragment() {
+
+    private lateinit var feedListViewModel: FeedListViewModel
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        feedListViewModel =
+            ViewModelProviders.of(this).get(FeedListViewModel::class.java)
+    }
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -15,5 +25,9 @@ class FeedListFragment : Fragment(){
         savedInstanceState: Bundle?
     ): View? {
         return inflater.inflate(R.layout.fragment_feed_list, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
     }
 }
--- app/src/main/java/com/nav/telstra/features/feedlist/viewModel/FeedListViewModel.kt
+++ app/src/main/java/com/nav/telstra/features/feedlist/viewModel/FeedListViewModel.kt
@@ -1,7 +1,41 @@
 package com.nav.telstra.features.feedlist.viewModel
 
 import android.app.Application
+import androidx.lifecycle.MutableLiveData
 import com.nav.telstra.base.BaseViewModel
+import com.nav.telstra.features.feedlist.model.FeedResponse
+import com.nav.telstra.network.ApiService
+import kotlinx.coroutines.*
+import javax.inject.Inject
 
 class FeedListViewModel(application: Application) : BaseViewModel(application) {
+
+    @Inject
+    lateinit var apiService: ApiService
+
+    private var job: Job? = null
+    private val exceptionHandler = CoroutineExceptionHandler { _, throwable ->
+        print("Error: ${throwable.localizedMessage}")
+        errorMsg.value = throwable.localizedMessage
+    }
+
+    val errorMsg = MutableLiveData<String>()
+    val loading = MutableLiveData<Boolean>()
+    val feedData = MutableLiveData<FeedResponse>()
+
+    fun fetchFeedList() {
+        loading.value = true
+        job = CoroutineScope(Dispatchers.IO + exceptionHandler).launch {
+            val apiResponse = apiService.getFeedList()
+            if (apiResponse.isSuccessful) {
+                withContext(Dispatchers.Main) {
+                    loading.value = false
+                    feedData.value = apiResponse.body()
+                }
+            } else {
+                print(apiResponse.errorBody())
+                errorMsg.value = apiResponse.errorBody().toString()
+            }
+        }
+    }
 }
