b2d2b9372c9e310954d105cd3936e90613d3919b
navneetsharma <Navneetsharma262@gmail.com>
2020-08-12 22:43:25 +0530

    Added FeedListFragment in MainActivity.

--- app/src/main/java/com/nav/telstra/MainActivity.kt
+++ app/src/main/java/com/nav/telstra/MainActivity.kt
@@ -2,10 +2,14 @@ package com.nav.telstra
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.nav.telstra.features.feedlist.view.FeedListFragment
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        supportFragmentManager.beginTransaction()
+            .add(R.id.fragment_container_view, FeedListFragment())
+            .commit()
     }
 }
--- app/src/main/java/com/nav/telstra/features/feedlist/view/FeedListFragment.kt
+++ app/src/main/java/com/nav/telstra/features/feedlist/view/FeedListFragment.kt
@@ -1,4 +1,19 @@
 package com.nav.telstra.features.feedlist.view
 
-class FeedListFragment {
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.nav.telstra.R
+
+class FeedListFragment : Fragment(){
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.fragment_feed_list, container, false)
+    }
 }
